d12ededf322ee98a7ec704c5050ad201031a6f82
daniilmiagkov <59875675+daniilmiagkov@users.noreply.github.com>
2024-02-02 18:07:06 +0700

    add server

---
+++ server/server.cjs
@@ -0,0 +1,33 @@
+const express = require('express')
+const server = express()
+const fs = require('fs')
+const path = require('path')
+const projectHub = path.join(__dirname, '..');
+
+/*
+const file = path.join(__dirname, '..') + '\\dist\\index.html'
+*/
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next();
+})
+
+server.use('/dist', express.static(path.join(projectHub, 'dist')));
+
+server.use(express.static(path.join(__dirname, '.')));
+/*
+console.log(path.join(__dirname, '..'))
+*/
+server.get('/', function(req, res) {
+  res.set('Content-Type', 'text/html');
+  
+  fs.readFile(projectHub + '\\dist\\index.html', 'utf8', (err, data) => {
+    res.send(data);
+    
+  })
+})
+
+server.listen(3000, () => {
+  console.log('listening on port http://localhost:3000')
+})
