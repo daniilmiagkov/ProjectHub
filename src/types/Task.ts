e4eb7b4481fb4351bf1fda8fa260a828b45bc56b
daniilmiagkov <59875675+daniilmiagkov@users.noreply.github.com>
2024-02-03 09:33:01 +0700

    added classes for labs, tasks, and subjects

---
+++ src/types/Task.ts
@@ -0,0 +1,8 @@
+export class Task {
+    Title: string;
+    Done: boolean;
+    constructor(title: string, done: boolean) {
+        this.Title = title;
+        this.Done = done;
+    }
+}
---
+++ src/utils/createListSubjects.ts
@@ -0,0 +1,5 @@
+import {Subject} from "../types/Subject.ts";
+
+export function createListSubjects() {
+  const subjects: Array<Subject> = [];
+}
