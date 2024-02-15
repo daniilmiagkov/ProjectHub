06afc7e892598fb8f8f2e1fd9151fdc0ba92ee38
daniilmiagkov <59875675+daniilmiagkov@users.noreply.github.com>
2024-02-15 22:32:35 +0300

    added route processing to the server to change the lab and changed the structure of the lab

--- backend/server.cjs
+++ backend/server.cjs
@@ -3,15 +3,18 @@ const server = express()
 const fs = require('fs')
 const path = require('path')
 const projectHub = path.join(__dirname, '..');
+const bodyParser = require('body-parser');
 
 /*
 const file = path.join(__dirname, '..') + '\\dist\\index.html'
 */
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  // req.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next();
 })
+server.use(bodyParser.json());
 
 server.use('/dist', express.static(path.join(projectHub, 'dist')));
 
@@ -36,11 +39,43 @@ server.get('/database/names', (req, res) => {
   })
 })
 
-server.get(new RegExp('/database/db_\\w+/\\w+_subject.json'), (req, res) => {
+server.post('/database/lab', (req, res) => {
+  console.log(req.body);
+  const body = req.body;
+  fs.writeFile( projectHub + '\\backend'+ body.Path, JSON.stringify(body), () => {
+    console.log(projectHub + body.Path, "file overwritten")
+  });
+  res.status(200);
+  res.send();
+})
+
+server.get(new RegExp('/database/\\w+/\\w+_lab_\\w+'), (req, res) => {
   res.set('Content-Type', 'application/json');
   
   // Извлекаем параметры из пути запроса
-  const [, dbName, subject] = req.path.match(/\/database\/(db_\w+)\/(\w+)_subject.json/);
+  const [, dbName, subject, lab] = req.path.match(/\/database\/(\w+)\/(\w+)_lab_(\w+)/);
+  
+  // Собираем путь к файлу на основе параметров
+  const filePath = `${projectHub}/backend/database/${dbName}/${subject}_${lab}.json`;
+  
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      // Обработка ошибок, например, если файл не найден
+      console.error('Ошибка чтения файла:', err);
+      res.status(500).send('Internal Server Error');
+    } else {
+      res.send(data);
+      // console.log(data);
+    }
+  });
+});
+
+
+server.get(new RegExp('/database/\\w+/\\w+_subject'), (req, res) => {
+  res.set('Content-Type', 'application/json');
+  
+  // Извлекаем параметры из пути запроса
+  const [, dbName, subject] = req.path.match(/\/database\/(\w+)\/(\w+)_subject/);
   
   // Собираем путь к файлу на основе параметров
   const filePath = `${projectHub}/backend/database/${dbName}/${subject}_subject.json`;
@@ -51,8 +86,8 @@ server.get(new RegExp('/database/db_\\w+/\\w+_subject.json'), (req, res) => {
       console.error('Ошибка чтения файла:', err);
       res.status(500).send('Internal Server Error');
     } else {
+      // console.log(data);
       res.send(data);
-      console.log(data);
     }
   });
 });
